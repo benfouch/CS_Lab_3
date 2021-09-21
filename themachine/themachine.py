7ab5bccdc8b26eb305f66f8c0e5413d9df7fb8f8
Ben-Fouch <fouchb@msoe.edu>
2021-09-21 12:04:39 -0500

    Added comments

--- themachine/themachine.py
+++ themachine/themachine.py
@@ -1,11 +1,11 @@
 """
-- NOTE: REPLACE 'N' Below with your section, year, and lab number
-- CS2911 - 0NN
-- Fall 202N
-- Lab N
+- CS2911 - 011
+- Fall 2021
+- Lab 3
 - Names:
-  - 
-  - 
+  - Ben Fouch
+  - Nathan Cernik
+  - Aiden Regan
 
 "The Machine" implementation of parsing and executing a program.
 
@@ -15,15 +15,13 @@ The file format is describe in "The Machine" exercise and the lab description.
 
 
 Introduction: (Describe the lab in your own words)
-
-
-
+    This lab is designed to have us automate(with python), the process of decoding that we have been practicing by hand.
+    It helps teach us the pitfalls of dealing with bytes coming from files, and hows us how python can interpret these bytes for us.
 
 Summary: (Summarize your experience with the lab, what you learned, what you liked, what you disliked, and any suggestions you have for improvement)
-
-
-
-
+    Over all, the lab experience was very good. We were able to work as a team well to get the code done very quickly.
+    We learned how to deal with bytes objects, conversion between bytes objects, and a bit of python syntax/how python works
+    We have no suggestions for the lab, its great
 """
 
 import readfile
@@ -43,12 +41,15 @@ def execute(program_file):
     """
     - Execute a program file formatted for "The Machine"
     :param str program_file: name of the file to execute
+    :author Ben Fouch
     """
 
     try:
         readfile.set_file(program_file)
         if check_magic_number(readfile.read_byte() + readfile.read_byte() + readfile.read_byte() + readfile.read_byte()):
-            read_operand(read_num_of_ops(readfile.read_byte() + readfile.read_byte()))
+            perform_ops(read_num_of_ops(readfile.read_byte() + readfile.read_byte()))
+        else:
+            print("Invalid Header")
     except:
         print("Invalid File")
     finally:
@@ -56,14 +57,31 @@ def execute(program_file):
 
 
 def check_magic_number(header):
+    """
+    - Checks the first bytes of the file to verify that it meets the requirements of the file format
+    :param header the first 4 bytes of the file that should match the required format
+    :author Ben Fouch
+    :author Aiden Regan
+    """
     return header == b'1A\xfa\xce'
 
 
-def read_num_of_ops(next_byte):
-    return int.from_bytes(next_byte, "big")
+def read_num_of_ops(next_bytes):
+    """
+    - Finds the number of operations to be performed in the file
+    :param next_bytes the next 2 bytes from the file
+    :author Ben Fouch
+    :author Aiden Regan
+    """
+    return int.from_bytes(next_bytes, "big")
 
 
-def read_operand(num_of_ops):
+def perform_ops(num_of_ops):
+    """
+    - finds the value of the next operand and sends a helper method to perform it
+    :param num_of_ops the number of operation in the file as given from the read_num_of_ops method
+    :author Nathan Cernik
+    """
     while num_of_ops != 0:
         b1 = readfile.read_byte()
         opd = b1
@@ -86,30 +104,60 @@ def read_operand(num_of_ops):
 
 
 def add():
+    """
+    - performs the add operation
+    :author Nathan Cernik
+    :author Ben Fouch
+    :author Aiden Regan
+    """
     byte_1 = readfile.read_byte() + readfile.read_byte()
     byte_2 = readfile.read_byte() + readfile.read_byte()
     print(int.from_bytes(byte_1, "big") + int.from_bytes(byte_2, "big"))
 
 
 def subtract():
+    """
+    - performs the subtract operation
+    :author Nathan Cernik
+    :author Ben Fouch
+    :author Aiden Regan
+    """
     byte_1 = readfile.read_byte() + readfile.read_byte()
     byte_2 = readfile.read_byte() + readfile.read_byte()
     print(int.from_bytes(byte_1, "big") - int.from_bytes(byte_2, "big"))
 
 
 def multiply():
+    """
+    - performs the multiply operation
+    :author Nathan Cernik
+    :author Ben Fouch
+    :author Aiden Regan
+    """
     byte_1 = readfile.read_byte() + readfile.read_byte()
     byte_2 = readfile.read_byte() + readfile.read_byte()
     print(int.from_bytes(byte_1, "big") * int.from_bytes(byte_2, "big"))
 
 
 def divide():
+    """
+    - performs the divide operation
+    :author Nathan Cernik
+    :author Ben Fouch
+    :author Aiden Regan
+    """
     byte_1 = readfile.read_byte() + readfile.read_byte()
     byte_2 = readfile.read_byte() + readfile.read_byte()
     print(int.from_bytes(byte_1, "big") / int.from_bytes(byte_2, "big"))
 
 
 def string():
+    """
+     - Prints out the ascii value of the next bytes until the hex char '0x0A' is found
+    :author Nathan Cernik
+    :author Ben Fouch
+    :author Aiden Regan
+    """
     next_byte = readfile.read_byte()
     end_string = ""
     while next_byte != b'\x0A':
